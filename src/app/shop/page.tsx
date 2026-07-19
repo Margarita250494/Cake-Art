23ff74bd379068315a8b9b412cfca972c6fd5bed
margaritaboiko <margo250494@gmail.com>
2026-07-19 13:05:16 +0200

    disable shop cache for dynamic products

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,6 +1,7 @@
 import ShopClient from "@/app/shop/ShopClient";
 import { getProducts } from "@/lib/getProducts";
 
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const products = await getProducts();
   console.log("SHOP PRODUCTS:", products);
